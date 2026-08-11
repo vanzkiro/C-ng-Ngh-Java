5747a3f0b575b54e152d5978d627166f7b6ff498
vanzkiro <loveyouanh0147@gmail.com>
2026-08-11 13:02:21 +0700

    Bai moi

---
+++ Chuong2/src/main/java/com/mycompany/chuong2/Chuong2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chuong2;
+
+/**
+ *
+ * @author DELL
+ */
+public class Chuong2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
